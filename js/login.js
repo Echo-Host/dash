bde20ffb14ff7dba5c6a6171f3509be02c4d37fd
Sahran <94926019+SahranREAL@users.noreply.github.com>
2024-09-21 21:10:15 +0200

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,9 @@
+(function() {
+    // Vérifie si l'utilisateur est authentifié
+    const isAuthenticated = localStorage.getItem('isAuthenticated');
+
+    if (isAuthenticated !== 'true') {
+        // Redirection vers la page de connexion si non authentifié
+        window.location.href = 'login.html';
+    }
+})();
