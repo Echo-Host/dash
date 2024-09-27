fb3fbc1980805a3d89e5a1733b5697fa6cdecf44
Sahran <94926019+SahranREAL@users.noreply.github.com>
2024-09-27 17:04:25 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-⁸document.querySelectorAll('.sidebar-submenu').forEach(e => {
+document.querySelectorAll('.sidebar-submenu').forEach(e => {
     e.querySelector('.sidebar-menu-dropdown').onclick = (event) => {
         event.preventDefault()
         e.querySelector('.sidebar-menu-dropdown .dropdown-icon').classList.toggle('active')
